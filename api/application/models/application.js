f04ba78f4e22629a9308306af8805886c6a85036
Manny Laporte <mannylaporte@gmail.com>
2021-08-20 12:22:12 +0400

    Integration with CRM

--- api/application/models/application.js
+++ api/application/models/application.js
@@ -1,5 +1,6 @@
 'use strict';
 const fetch = require("node-fetch");
+const fs = require('fs');
 
 const _sugarURL = process.env.CRM_API_ENDPOINT;
 
@@ -22,7 +23,7 @@ async function getSugarToken() {
         };
         const _response = await fetch(`${_sugarURL}/oauth2/token`, _request);
         const _data = await _response.json();
-        if (_data && _data.access_token) {
+        if (_data && _data.access_token) { 
             _token = _data.access_token;
         }
     } catch(err) {
@@ -157,12 +158,10 @@ async function createContact(accessToken, form) {
 
 async function createContactIndentity(accessToken, form, contactId) {
     console.log('=======createContactIndentity======')
-
-    let _payloadList = [];
     try { 
         const _identifications = form.section4.identifications;
         if (_identifications && _identifications.length > 0) {
-            _identifications.forEach(function (item) {
+            _identifications.forEach(async function (item) {
                 let _payload = {
                     name: null,
                     identification_type_c: null,
@@ -170,47 +169,27 @@ async function createContactIndentity(accessToken, form, contactId) {
                     valid_from_c: null,
                     valid_to_c: null,
                     uuid_c: null,
-                    abs1_identifications_contactscontacts_ida: null,
                 }
                 _payload.name = item && item.number ? item.number : 'unknown';
                 _payload.identification_type_c = item && item.type && item.type.code ? item.type.code : null;
                 _payload.issuing_country_c = item && item.country ? item.country : null;
-                _payload.uuid = item && item.uuid ? item.uuid : null;
-                _payload.abs1_identifications_contactscontacts_ida = contactId; // TODO: test if it works
-                _payloadList.push(_payload);
-            });
-        }
-        console.log('==========_payloadList final===================')
-        console.log(_payloadList);
-        if (_payloadList && _payloadList.length > 0) {
-            _payloadList.forEach(async function (item) {
-                console.log('==========item=================')
-                console.log(item);
+                if (item && item.valid_until) {
+                    var _parts = item.valid_until.split('/');
+                    // _parts[2] = YYYY, _parts[1] = MM, _parts[0] = DD - notice the month (_parts[1]); JS counts months from 0:
+                    _payload.valid_to_c = new Date(_parts[2], _parts[1] - 1, _parts[0]);
+                }
+                _payload.uuid_c = item && item.uuid ? item.uuid : null;
                 const _request = {
                     method: 'POST',
                     headers: {
                         'OAuth-Token': accessToken,
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({item}),
+                    body: JSON.stringify(_payload)
                 };
-                const _response = await fetch(`${_sugarURL}/ABS1_identifications`, _request);
-                //const _data = await _response.json();
-                // console.log(_data)
+                await fetch(`${_sugarURL}/Contacts/${contactId}/link/abs1_identifications_contacts`, _request);
             });
-        };
-        /*
-        const _request = {
-            method: 'POST',
-            headers: {
-                'OAuth-Token': accessToken,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({_payloadList}),
-        };
-        const _response = await fetch(`${_sugarURL}/ABS1_identifications`, _request);
-        const _data = await _response.json(); */
-        // console.log(_data)
+        }
     } catch(err) {
         console.log(err);
     }
@@ -339,30 +318,186 @@ async function linkContactToAccount(accessToken, contactId, accountId) {
     }
 }
 
+async function createApplication(accessToken, form, contactId, accountId, refnum) {
+    console.log('=======createApplication======')
+    let _id = null;
+    let _payload = {
+        name: refnum,
+        uuid_c: null,
+        purpose_c: null,
+        applicant_type_c: null,
+        account_id: accountId,
+        contact_id_c: contactId,
+        contact_role: 'Natural Person',
+        sales_stage: 'New Submission',
+        priority_c: 'Medium',
+        started_date_c: null,
+        submitted_date_c: null,
+        expected_completion_date_c: null,
+        np_capacity_c: null,
+        indigenous_involvement_c: null,
+        knowledge_use_c: null,
+        agree_colla_prty_c: null,
+        agree_resource_prvd_c: null,
+        agree_resource_owner_c: null,
+        assist_authority_c: null,
+        assist_requested_c: null,
+        form_declaration_c: null,
+    }
+    try {
+        const _summary = form.summary;
+        if (_summary && _summary.uuid) {
+             _payload.uuid_c = _summary.uuid;
+        }
 
-async function sendToCRM(form) {
+        const _purpose = form.section2.purpose;
+        if (_purpose) {
+            switch(_purpose.code) {
+                case "commercial": {
+                    _payload.purpose_c = 'Commercial Use';
+                    break;
+                }
+                case "non-commercial": {
+                    _payload.purpose_c = 'Non Commercial Use';
+                    break;
+                }
+                default: {
+                    _payload.purpose_c = null;
+                    break;
+                }
+            }
+        }
+
+        const _type = form.section3.personType;
+        if (_type) {
+            switch(_type.code) {
+                case "naturalperson": {
+                    _payload.applicant_type_c = 'Natural Person';
+                    break;
+                }
+                case "legalperson": {
+                    _payload.applicant_type_c = 'Legal Person';
+                    break;
+                }
+                default: {
+                    _payload.applicant_type_c = null;
+                    break;
+                }
+            }
+        }
+
+        const _date_started = form.summary.startdate;
+        if (_date_started) {
+            _payload.started_date_c = _date_started;
+        }
+
+        const _submitdate = form.summary.submitdate;
+        if (_submitdate) {
+            var _sdt = new Date(_submitdate);
+            var _ecdt = new Date();
+            _ecdt.setMonth(_sdt.getMonth() + 1);
+            _payload.submitted_date_c = _sdt;
+            _payload.expected_completion_date_c = _ecdt;
+        }
+
+        const _contactcapacity = form.section8.contactcapacity;
+        if (_contactcapacity) {
+            _payload.np_capacity_c = _contactcapacity;
+        }
+
+        const _community = form.section17.community;
+        if (_community) {
+            _payload.indigenous_involvement_c = _community;
+        }
+
+        const _knowledge = form.section17.knowledge;
+        if (_knowledge) {
+            _payload.knowledge_use_c = _knowledge;
+        }
+
+        const _dis_collaborate = form.section18.agreementSigned;
+        if (_dis_collaborate && _dis_collaborate.id) {
+            _payload.agree_colla_prty_c = _dis_collaborate.id === 1 ? 'Yes' : 'No';
+        }
+
+        const _dis_provider = form.section18.disclosedToProviders;
+        if (_dis_provider && _dis_provider.id) {
+            _payload.agree_resource_prvd_c = _dis_provider.id === 1 ? 'Yes' : 'No';
+        }
+
+        const _dis_holder = form.section18.disclosedToHolders;
+        if (_dis_holder && _dis_holder.id) {
+            _payload.agree_resource_owner_c = _dis_holder.id === 1 ? 'Yes' : 'No';
+        }
+
+        const _assist_authority = form.section18.assistanceNeeded;
+        if (_assist_authority && _assist_authority.id) {
+            _payload.assist_authority_c = _assist_authority.id === 1 ? 'Yes' : 'No';
+        }
+
+        
+        const _assist_note = form.section18.assistanceNote;
+        if (_assist_note) {
+            _payload.assist_requested_c = _assist_note.trim();;
+        }
+
+        const _dis_agreed = form.section20.agreed;
+        _payload.form_declaration_c = _dis_agreed ? true : false;
+
+        const _request = {
+            method: 'POST',
+            headers: {
+                'OAuth-Token': accessToken,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(_payload),
+        };
+        const _response = await fetch(`${_sugarURL}/Contacts/${contactId}/link/opportunities`, _request);
+        const _data = await _response.json();
+        if (_data && _data.related_record && _data.related_record.id) {
+            _id = _data.related_record.id;
+        }
+    } catch(err) {
+        console.log(err);
+    }
+    return _id;
+}
+
+async function sendToCRM(entry) {
+    console.log('=======sendToCRM======')
+    const _form = entry.form;
+    let _accessToken = null;
     let _newContact = false;
     let _contactId = null;
     let _accountId = null;
-    const _contactEmail = form.section4 && form.section4.email && form.section4.email.address ? form.section4.email.address : null;
-    const _accountEmail = form.section6 && form.section6.email && form.section6.email.address ? form.section6.email.address : null;
-    const _accessToken = await getSugarToken();
+    const _contactEmail = _form.section4 && _form.section4.email && _form.section4.email.address ? _form.section4.email.address : null;
+    const _accountEmail = _form.section6 && _form.section6.email && _form.section6.email.address ? _form.section6.email.address : null;
+    _accessToken = await getSugarToken();
     if (_contactEmail) {
         _contactId = await getRecordIdByEmail(_accessToken, _contactEmail, 'contacts');
+        console.log()
         if (!_contactId) {
             _newContact = true;
-            _contactId = await createContact(_accessToken, form);
-            /// await createContactIndentity(_accessToken, form, _contactId); //TODO: add
+            _contactId = await createContact(_accessToken, _form);
         }
     }
     if (_accountEmail) {
         _accountId = await getRecordIdByEmail(_accessToken, _accountEmail, 'accounts');
         if (!_accountId) {
-            _accountId = await createAccount(_accessToken, form);
+            _accountId = await createAccount(_accessToken, _form);
         }
     }
     if (_newContact) {
-        await linkContactToAccount(_accessToken, _contactId, _accountId)
+        await linkContactToAccount(_accessToken, _contactId, _accountId);
+        //await createContactIndentity(_accessToken, _form, _contactId);
+    }
+    await createContactIndentity(_accessToken, _form, _contactId);
+    const _refnum = entry.refnum;
+    if (_refnum) {
+        const _opptyId = await createApplication(_accessToken, _form, _contactId, _accountId, _refnum)
+        console.log('========_opptyId=========' + _opptyId)
+        //await linkContactToApplication(_accessToken, _contactId, _opptyId);
+
     }
 }
 
@@ -373,8 +508,9 @@ module.exports = {
             console.log('---------------afterUpdate---------------');
             const _form = entry.form ? entry.form : null;
             console.log(entry.status); //TODO: add && _form.status != draft
+            console.log(entry.refnum);
             if (_form) { //TODO: add && _form.status != draft
-                await sendToCRM(_form);
+                await sendToCRM(entry);
             }
         },
     }
